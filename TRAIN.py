06c64a24519c3e2140f084d486516337c732e11f
Samuel Adegboyega <gboyebolt@gmail.com>
2025-03-17 12:50:34 +0100

    Reduce training epochs from 20 to 10 for optimized training duration

--- TRAIN.py
+++ TRAIN.py
@@ -234,7 +234,7 @@ if __name__=="__main__":
         'DECODER_MLP_ratio': 2,
         'VOCAB_SIZE':len(tokenizer),
         'lr':0.0001,
-        'EPOCHS':20,
+        'EPOCHS':10,
         'SEED':0,
         'TRAIN_PATH':os.path.join(os.getcwd(),'DATA/Train.csv'),
         'VALID_PATH':os.path.join(os.getcwd(),'DATA/Train.csv'),
